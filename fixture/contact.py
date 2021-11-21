2598b614e492837a07ede7b4fc107d17020a43a7
osavchik <okssav2004@gmail.com>
2021-11-21 23:36:48 +0300

    Задание №10: оптимизировать переходы между страницами.

--- fixture/contact.py
+++ fixture/contact.py
@@ -40,7 +40,8 @@ class ContactHelper:
 
     def return_Home(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def delete_first_contact(self):
         wd = self.app.wd
--- fixture/group.py
+++ fixture/group.py
@@ -5,25 +5,30 @@ class GroupHelper:
 
     def create(self, group):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        self.open_group_page()
         wd.find_element_by_name("new").click()
         # Fill form for group
         self.fill_group_form(group)
         # Submit Click
         wd.find_element_by_name("submit").click()
         # return Group Page
-        wd.find_element_by_link_text("groups").click()
+        self.open_group_page()
+
+    def open_group_page(self):
+        wd = self.app.wd
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new"))>0):
+            wd.find_element_by_link_text("groups").click()
 
     def delete_first_group(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        self.open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("delete").click()
         wd.find_element_by_link_text("group page").click()
 
     def edit_first_group(self, new_group_data):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        self.open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
         # Fill form for group
@@ -31,7 +36,7 @@ class GroupHelper:
         # Update Click
         wd.find_element_by_name("update").click()
         # return Group Page
-        wd.find_element_by_link_text("groups").click()
+        self.open_group_page()
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -48,7 +53,7 @@ class GroupHelper:
 
     def count(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
 
 
