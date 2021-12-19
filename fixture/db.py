8505c56f472eebc682ca93b4f373c40460a32665
osavchik <okssav2004@gmail.com>
2021-12-19 23:22:26 +0300

    Выполнение Задания 21

--- fixture/db.py
+++ fixture/db.py
@@ -26,7 +26,7 @@ class DbFixture:
         list = []
         cursor =  self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, middlename, lastname, nickname from addressbook")
+            cursor.execute("select id, firstname, middlename, lastname, nickname from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, first_name, middle_name, last_name, nick_name) = row
                 list.append(Contact(id=str(id), first_name=first_name, middle_name=middle_name, last_name=last_name, nick_name=nick_name))
