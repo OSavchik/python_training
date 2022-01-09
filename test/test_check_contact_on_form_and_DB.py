ca646ddc8abc8e6ff042b0b07543f21980836ecb
osavchik <okssav2004@gmail.com>
2022-01-09 23:13:08 +0300

    Корректировка задания 21 по замечаниям

--- test/test_check_contact_on_form_and_DB.py
+++ test/test_check_contact_on_form_and_DB.py
@@ -13,12 +13,24 @@ def test_check_contact_on_form_and_DB(app, db):
         assert str(contact_from_form.first_name).strip() == str(contact_from_DB.first_name).strip()
         assert str(contact_from_form.last_name).strip() == str(contact_from_DB.last_name).strip()
         assert str(contact_from_form.address_name).strip() == str(contact_from_DB.address_name).strip()
-        email_db_form = [contact_from_DB.email, contact_from_DB.email2, contact_from_DB.email3]
-        assert contact_from_form.all_email == app.contact.merge_fields_on_home_page(email_db_form)
-        all_phones_db_form = [contact_from_DB.home_phone, contact_from_DB.mobil_phone,
-                                contact_from_DB.work_phone, contact_from_DB.secondary_phone]
-        assert contact_from_form.all_phones_from_home_page == app.contact.merge_fields_on_home_page(all_phones_db_form)
+        assert contact_from_form.all_email.replace(' ', '') == merge_emails_like_on_home_page(contact_from_DB).replace(' ', '')
+        assert contact_from_form.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_DB)
         element = element + 1
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_emails_like_on_home_page(contact):
+        return "\n".join(filter(lambda x: x != "",
+                                filter(lambda x: x is not None,
+                                       [contact.email, contact.email2, contact.email3])))
+
+def merge_phones_like_on_home_page(contact):
+        return "\n".join(filter(lambda x: x != "",
+                                map(lambda x: clear(x),
+                                    filter(lambda x: x is not None,
+                                           [contact.home_phone, contact.mobil_phone, contact.work_phone,
+                                            contact.secondary_phone]))))
 
 
 
